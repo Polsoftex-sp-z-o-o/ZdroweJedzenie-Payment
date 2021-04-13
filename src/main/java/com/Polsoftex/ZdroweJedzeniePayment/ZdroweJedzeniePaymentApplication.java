fb3e300bd1e3f344be2a0ffea8afacd368aecbf2
Atloas <miter010@gmail.com>
2021-04-13 23:42:26 +0200

    CI/CD

---
+++ src/main/java/com/Polsoftex/ZdroweJedzeniePayment/ZdroweJedzeniePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.Polsoftex.ZdroweJedzeniePayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdroweJedzeniePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdroweJedzeniePaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Polsoftex/ZdroweJedzeniePayment/ZdroweJedzeniePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Polsoftex.ZdroweJedzeniePayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdroweJedzeniePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
