160629fde8b6e1a0d82519c35c3ae23ece998296
Atloas <miter010@gmail.com>
2021-04-22 14:38:03 +0200

    Card validation

--- src/main/java/com/Polsoftex/ZdroweJedzeniePayment/services/PaymentService.java
+++ src/main/java/com/Polsoftex/ZdroweJedzeniePayment/services/PaymentService.java
@@ -2,8 +2,13 @@ package com.Polsoftex.ZdroweJedzeniePayment.services;
 
 import com.Polsoftex.ZdroweJedzeniePayment.model.Card;
 import com.Polsoftex.ZdroweJedzeniePayment.services.exceptions.CardInvalidException;
+import org.apache.commons.validator.routines.CreditCardValidator;
 import org.springframework.stereotype.Service;
 
+import java.time.YearMonth;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -19,14 +24,36 @@ public class PaymentService {
 
     private void sendPaymentRequest() {
         try {
-            TimeUnit.SECONDS.sleep(5);
+            TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
             e.getMessage();
         }
     }
 
     private boolean validateCardData(Card card) {
-        // TODO
-        return true;
+        boolean numberValid = validateCardNumber(card.getNumber());
+        boolean dateValid = validateCardExpirationDate(card.getExpiration());
+        return numberValid && dateValid;
+    }
+
+    private boolean validateCardNumber(String number) {
+        CreditCardValidator validator = new CreditCardValidator();
+        return validator.isValid(number);
+    }
+
+    private boolean validateCardExpirationDate(String expiration) {
+        DateTimeFormatter ccMonthFormatter = DateTimeFormatter.ofPattern("MM/uu");
+        YearMonth lastValidMonth;
+
+        // Check if the expiration date is in a valid format
+        try {
+            lastValidMonth = YearMonth.parse(expiration, ccMonthFormatter);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+
+        // Check if the card is not expired
+        YearMonth now = YearMonth.now(ZoneOffset.UTC);
+        return !now.isAfter(lastValidMonth);
     }
 }
